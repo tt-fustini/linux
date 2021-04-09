aae2f09f52e71dc83f8ab3f29edb729bb75d0eca
James Morse <james.morse@arm.com>
2021-04-09 19:12:36 +0100

    irqchip/gic, gic-v3: Translate fwspec for DT and ACPI systems in the same way
    gic_irq_domain_translate() currently rejects any fwspec for ACPI
    systems that has more than 2 paramters.
    
    ACPI only provides a hwirq and type via its fwspec, there isn't an
    extra GIC specific field that can be overriden to describe a new
    partition as __partition_ppis() does with GIC_IRQ_TYPE_PARTITION.
    
    Enable ACPI_IRQCHIP_FWSPEC_ARG0, to make all calls from
    acpi_register_gsi() pass GIC_IRQ_TYPE_GSI as their param[0].
    This makes the ACPI format match DT, and allows the irqchip driver
    to translate fwspec from acpi_register_gsi() without knowing its
    in 'acpi mode'.
    
    its_irq_gic_domain_alloc() is one example of these, it no longer needs
    to produce a fwspec in two different layouts.
    
    arm64 systems booting with ACPI only support gicv2 or later, this
    doesn't need enabling to support other irqchip drivers.
    32bit arm systems support gicv2, but don't use ACPI.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- drivers/irqchip/irq-gic.c
+++ drivers/irqchip/irq-gic.c
@@ -1096,7 +1096,9 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 		return 0;
 	}
 
-	if (is_of_node(fwspec->fwnode)) {
+
+	if (is_of_node(fwspec->fwnode) ||
+	    is_fwnode_irqchip(fwspec->fwnode)) {
 		if (fwspec->param_count < 3)
 			return -EINVAL;
 
@@ -1107,6 +1109,17 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 		case 1:			/* PPI */
 			*hwirq = fwspec->param[1] + 16;
 			break;
+#ifdef GIC_IRQ_TYPE_GSI
+		case GIC_IRQ_TYPE_GSI:	/* GSI */
+			if (fwspec->param[1] < 16) {
+				pr_err(FW_BUG "Illegal GSI%d translation request\n",
+				       fwspec->param[0]);
+				return -EINVAL;
+			}
+
+			*hwirq = fwspec->param[1];
+			break;
+#endif /* GIC_IRQ_TYPE_GSI */
 		default:
 			return -EINVAL;
 		}
@@ -1114,24 +1127,6 @@ static int gic_irq_domain_translate(struct irq_domain *d,
 		*type = fwspec->param[2] & IRQ_TYPE_SENSE_MASK;
 
 		/* Make it clear that broken DTs are... broken */
-		WARN(*type == IRQ_TYPE_NONE,
-		     "HW irq %ld has invalid type\n", *hwirq);
-		return 0;
-	}
-
-	if (is_fwnode_irqchip(fwspec->fwnode)) {
-		if(fwspec->param_count != 2)
-			return -EINVAL;
-
-		if (fwspec->param[0] < 16) {
-			pr_err(FW_BUG "Illegal GSI%d translation request\n",
-			       fwspec->param[0]);
-			return -EINVAL;
-		}
-
-		*hwirq = fwspec->param[0];
-		*type = fwspec->param[1];
-
 		WARN(*type == IRQ_TYPE_NONE,
 		     "HW irq %ld has invalid type\n", *hwirq);
 		return 0;
