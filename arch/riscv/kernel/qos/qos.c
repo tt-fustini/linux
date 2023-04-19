9c98ee2f32e6673d4f47bbf994d055e8e26531b9
Drew Fustini <fustini@kernel.org>
2023-04-19 04:10:59 -0700

    RISC-V: QoS: add late_initcall to setup resctrl interface
    Add late_initcall which checks if the Ssqosid extension is present, and
    if so, calls resctrl setup and sets cpu hotplug state to "qos:online".
    
    Co-developed-by: Adrien Ricciardi <aricciardi@baylibre.com>
    Signed-off-by: Adrien Ricciardi <aricciardi@baylibre.com>
    Signed-off-by: Drew Fustini <fustini@kernel.org>

--- arch/riscv/kernel/qos/qos.c
+++ arch/riscv/kernel/qos/qos.c
@@ -1,5 +1,32 @@
 // SPDX-License-Identifier: GPL-2.0-only
+#include <linux/slab.h>
+#include <linux/err.h>
+#include <linux/cpu.h>
+#include <linux/cpumask.h>
+#include <linux/riscv_qos.h>
+
+#include <asm/csr.h>
 #include <asm/qos.h>
+
+#include "internal.h"
 
 /* cached value of sqoscfg csr for each cpu */
 DEFINE_PER_CPU(u32, cpu_srmcfg);
+
+static int __init qos_arch_late_init(void)
+{
+	int err;
+
+	if (!riscv_isa_extension_available(NULL, SSQOSID))
+		return -ENODEV;
+
+	err = qos_resctrl_setup();
+	if (err != 0)
+		return err;
+
+	cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "qos:online", qos_resctrl_online_cpu,
+			  qos_resctrl_offline_cpu);
+
+	return err;
+}
+late_initcall(qos_arch_late_init);
