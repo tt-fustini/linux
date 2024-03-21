e0abeed173a99aca647813f6db3b69fc676411c7
James Morse <james.morse@arm.com>
2024-03-21 12:15:36 +0000

    ACPI / PPTT: Repainted cache ids from firmware to be less ugly
    ACPI added the 'Cache ID' field to the PPTT cache structures to allow
    caches to be referred to by other APCI tables. MPAM is an example of
    another table that points to caches using this ID. The ID values also
    get exposed to user-space via sysfs.
    
    While the cache topology has to be provided to the UEFI firmware as
    it can't be detected, the UEFI folk decided to generate the Cache IDs
    in such a way that they will be unique. This means they are large
    multi-digit numbers, even on a system with a single-digit number of
    CPUs. This is less than ideal as these values are exposed to user-space,
    and the user has to sometimes type them back in.
    
    As there is no other user of these values, repaint them to be in the
    range 0..N. Any helpers that take a cache_id need to specify if it
    comes from struct cacheinfo, and if so, unpaint it.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1506,6 +1506,7 @@ int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
 int find_acpi_cache_level_from_id(u32 cache_id);
 int acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
 int acpi_pptt_get_cpumask_from_cache_id(u32 cache_id, cpumask_t *cpus);
+u32 pptt_get_repainted_cache_id(u32 orig);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1540,6 +1541,10 @@ static inline int acpi_pptt_get_cpumask_from_cache_id(u32 cache_id,
 						      cpumask_t *cpus)
 {
 	return -EINVAL;
+}
+static inline u32 pptt_get_repainted_cache_id(u32 orig)
+{
+	return orig;
 }
 #endif
 
