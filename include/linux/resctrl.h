2d70e3e4e4269833005f0d884595beea512e9935
James Morse <james.morse@arm.com>
2024-06-03 18:21:25 +0100

    x86/resctrl: Use schema type to determine how to parse schema values
    Resctrl's architecture code gets to specify a function pointer that is
    used when parsing schema entries. This is expected to be one of two
    helpers from the filesystem code.
    
    Setting this function pointer allows the architecture code to change
    the ABI resctrl presents to user-space, and forces resctrl to expose
    these helpers.
    
    Instead, use the schema format enum to choose which schema parser to
    use. This allows the helpers to be made static and the structs used
    for passing arguments moved out of shared headers.
    
    Signed-off-by: James Morse <james.morse@arm.com>
    ---
    Changes since v3:
     * Removed a spurious semicolon
    
    Changes since v2:
     * This patch is new

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -157,7 +157,6 @@ struct resctrl_membw {
 	u32				*mb_map;
 };
 
-struct rdt_parse_data;
 struct resctrl_schema;
 
 /**
@@ -186,7 +185,6 @@ enum resctrl_schema_fmt {
  * @data_width:		Character width of data when displaying
  * @default_ctrl:	Specifies default cache cbm or memory B/W percent.
  * @format_str:		Per resource format string to show domain value
- * @parse_ctrlval:	Per resource function pointer to parse control values
  * @schema_fmt:	Which format string and parser is used for this schema.
  * @evt_list:		List of monitoring events
  * @cdp_capable:	Is the CDP feature available on this resource
@@ -204,9 +202,6 @@ struct rdt_resource {
 	int			data_width;
 	u32			default_ctrl;
 	const char		*format_str;
-	int			(*parse_ctrlval)(struct rdt_parse_data *data,
-						 struct resctrl_schema *s,
-						 struct rdt_domain *d);
 	enum resctrl_schema_fmt	schema_fmt;
 	struct list_head	evt_list;
 	bool			cdp_capable;
