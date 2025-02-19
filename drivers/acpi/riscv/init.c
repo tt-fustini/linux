59f061bb160a8e147f4d2b56b42c4d9e9ab4c12d
Drew Fustini <fustini@kernel.org>
2025-02-19 13:20:47 -0800

    acpi: riscv: Add support for RISC-V Quality of Service Controller (RQSC)
    Enable support for the RQSC table which describes the capacity and
    bandwidth QoS (CBQRI) controllers in a system.
    
    Link: https://github.com/riscv-non-isa/riscv-cbqri/releases/tag/v1.0
    Link: https://github.com/riscv-non-isa/riscv-rqsc/blob/main/src/
    Signed-off-by: Drew Fustini <fustini@kernel.org>

--- drivers/acpi/riscv/init.c
+++ drivers/acpi/riscv/init.c
@@ -6,10 +6,29 @@
 
 #include <linux/acpi.h>
 #include "init.h"
+#include <linux/riscv_qos.h>
 
 void __init acpi_arch_init(void)
 {
+	struct acpi_table_header *rqsc;
+	acpi_status status;
+	int rc;
+
 	riscv_acpi_init_gsi_mapping();
+
 	if (IS_ENABLED(CONFIG_ACPI_RIMT))
 		riscv_acpi_rimt_init();
+
+	if (!acpi_disabled) {
+		status = acpi_get_table(ACPI_SIG_RQSC, 0, &rqsc);
+		if (ACPI_FAILURE(status)) {
+			pr_err("%s(): failed to find ACPI RQSC table: %d", __func__,
+			       ACPI_FAILURE(status));
+		} else {
+			rc = acpi_parse_rqsc(rqsc);
+			if (rc < 0)
+				pr_err("%s(): failed to parse ACPI RQSC table: %d", __func__, rc);
+		}
+		acpi_put_table((struct acpi_table_header *)rqsc);
+	}
 }
