3d9574c7d59d43090d4b349fccad4f86c185ff81
Drew Fustini <fustini@kernel.org>
2025-09-10 17:12:09 -0700

    DO_NOT_MERGE riscv: qos: print trace information when changing sqoscfg

--- arch/riscv/include/asm/qos.h
+++ arch/riscv/include/asm/qos.h
@@ -13,13 +13,28 @@
 
 static inline void __switch_to_srmcfg(struct task_struct *prev, struct task_struct *next)
 {
-	u32 prev_srmcfg, next_srmcfg;
+	u32 csr_srmcfg, prev_srmcfg, next_srmcfg;
 
 	prev_srmcfg = READ_ONCE(prev->thread.srmcfg);
 	next_srmcfg = READ_ONCE(next->thread.srmcfg);
+	csr_srmcfg = csr_read(CSR_SRMCFG);
+
+	if(csr_srmcfg != prev_srmcfg)
+	{
+		trace_printk("WARNING CSR_SRCFG = 0x%02x \t prev_srmcfg: 0x%02x \t next_srmcfg: 0x%02x\n", csr_srmcfg, prev_srmcfg, next_srmcfg);
+		pr_err("WARNING CSR_SRCFG = 0x%02x \t prev_srmcfg: 0x%02x \t next_srmcfg: 0x%02x\n", csr_srmcfg, prev_srmcfg, next_srmcfg);
+	}
+
+	if ( prev_srmcfg != csr_srmcfg )
+	{
+		//trace_printk("CSR_SRCFG = 0x%02x \t prev: %03d [0x%02x] srmcfg: 0x%02x \t next: %03d [0x%02x] srmcfg: 0x%02x\n", csr_srmcfg, prev->pid, prev->pid, prev_srmcfg, next->pid, next->pid, next_srmcfg);
+	}
 
 	if (next_srmcfg != prev_srmcfg)
+	{
+		//trace_printk("next_srmcfg != prev_srmcfg; write next_srmcfg 0x%02x to CSR_SRMCFG\n", next_srmcfg);
 		csr_write(CSR_SRMCFG, next_srmcfg);
+	}
 }
 
 static __always_inline bool has_srmcfg(void)
