1c3d6aab546b41f3369b1a5923c659d34730c3ce
Drew Fustini <fustini@kernel.org>
2026-03-02 08:28:43 -0800

    include: riscv_qos.h: add struct cbqri_controller
    Add structs cbqri_controller, riscv_cbqri_bandwidth_caps,
    riscv_cbqri_capacity_caps to make it available to acpi
    rqsc driver.

--- include/linux/riscv_qos.h
+++ include/linux/riscv_qos.h
@@ -37,6 +37,66 @@ struct cbqri_controller_info {
 	} mem;
 };
 
+/* Capacity Controller hardware capabilities */
+struct riscv_cbqri_capacity_caps {
+        u16 ncblks; /* number of capacity blocks */
+        u16 cache_level;
+        u32 blk_size;
+
+        bool supports_alloc_at_data;
+        bool supports_alloc_at_code;
+
+        bool supports_alloc_op_config_limit;
+        bool supports_alloc_op_read_limit;
+        bool supports_alloc_op_flush_rcid;
+
+        bool supports_mon_at_data;
+        bool supports_mon_at_code;
+
+        bool supports_mon_op_config_event;
+        bool supports_mon_op_read_counter;
+
+        bool supports_mon_evt_id_none;
+        bool supports_mon_evt_id_occupancy;
+};
+
+/* Bandwidth Controller hardware capabilities */
+struct riscv_cbqri_bandwidth_caps {
+        u16 nbwblks; /* number of bandwidth blocks */
+        u16 mrbwb;   /* max reserved bw blocks */
+
+        bool supports_alloc_at_data;
+        bool supports_alloc_at_code;
+
+        bool supports_alloc_op_config_limit;
+        bool supports_alloc_op_read_limit;
+
+        bool supports_mon_at_data;
+        bool supports_mon_at_code;
+
+        bool supports_mon_op_config_event;
+        bool supports_mon_op_read_counter;
+
+        bool supports_mon_evt_id_none;
+        bool supports_mon_evt_id_rdwr_count;
+        bool supports_mon_evt_id_rdonly_count;
+        bool supports_mon_evt_id_wronly_count;
+};
+
+struct cbqri_controller {
+        struct cbqri_controller_info *ctrl_info;
+        void __iomem *base;
+
+        int ver_major;
+        int ver_minor;
+
+        struct riscv_cbqri_bandwidth_caps bc;
+        struct riscv_cbqri_capacity_caps cc;
+
+        bool alloc_capable;
+        bool mon_capable;
+};
+
 extern struct list_head cbqri_controllers;
 extern int cbqri_controllers_size;
 
