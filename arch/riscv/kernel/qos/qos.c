71f7838d7fff7ac707de7c74141eefa11c3db580
Drew Fustini <fustini@kernel.org>
2026-04-10 11:22:43 -0700

    RISC-V: QoS: enable resctrl support for Ssqosid
    Wire up the RISC-V QoS resctrl implementation:
    
    Add a late_initcall that checks for the Ssqosid extension and, if
    present, calls qos_resctrl_setup() to probe CBQRI controllers and
    initialize the resctrl filesystem, then registers CPU hotplug callbacks.
    
    Make CONFIG_RISCV_ISA_SSQOSID select ARCH_HAS_CPU_RESCTRL and
    RESCTRL_FS, and depends on MISC_FILESYSTEMS.
    
    Add qos_resctrl.o to the build when CONFIG_RISCV_ISA_SSQOSID is set.
    
    Co-developed-by: Adrien Ricciardi <aricciardi@baylibre.com>
    Signed-off-by: Adrien Ricciardi <aricciardi@baylibre.com>
    Signed-off-by: Drew Fustini <fustini@kernel.org>

--- arch/riscv/kernel/qos/qos.c
+++ arch/riscv/kernel/qos/qos.c
@@ -1,8 +1,40 @@
 // SPDX-License-Identifier: GPL-2.0-only
+#include <linux/slab.h>
+#include <linux/err.h>
+#include <linux/cpu.h>
+#include <linux/cpumask.h>
+#include <linux/riscv_qos.h>
+
+#include <asm/csr.h>
 #include <asm/qos.h>
+
+#include "internal.h"
 
 /* cached value of srmcfg csr for each cpu */
 DEFINE_PER_CPU(u32, cpu_srmcfg);
 
 /* default srmcfg value for each cpu, set via resctrl cpu assignment */
 DEFINE_PER_CPU(u32, cpu_srmcfg_default);
+
+static int __init qos_arch_late_init(void)
+{
+	int err;
+
+	if (!riscv_isa_extension_available(NULL, SSQOSID))
+		return -ENODEV;
+
+	err = qos_resctrl_setup();
+	if (err)
+		return err;
+
+	err = cpuhp_setup_state(CPUHP_AP_ONLINE_DYN, "qos:online",
+				qos_resctrl_online_cpu,
+				qos_resctrl_offline_cpu);
+	if (err < 0) {
+		resctrl_exit();
+		return err;
+	}
+
+	return 0;
+}
+late_initcall(qos_arch_late_init);
